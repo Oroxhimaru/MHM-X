047ed77c89a9ead3447241f584898a3b5ad1e2ec
Oroxhimaru <hm51562@gmail.com>
2024-06-02 15:10:23 +0500

    sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { HiHome } from 'react-icons/hi';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='flex flex-col gap-4 p-3'>
+      <Link href='/'>
+        <img src="./mhm-black.png" className='w-18 h-16 cursor-pointer p-3 hover:bg-gray-100 transition-all duration-200 ' />
+      </Link>
+      <Link
+        href='/'
+        className='flex items-center p-3 hover:bg-gray-100 rounded-full transition-all duration-200 gap-2 w-fit'
+      >
+        <HiHome className='w-7 h-7' />
+        <span className='font-bold hidden xl:inline'>Home</span>
+      </Link>
+      <button className='bg-blue-400 text-white rounded-full  hover:brightness-95 transition-all duration-200 w-48 h-9 shadow-md hidden xl:inline'>
+        Sign In
+      </button>
+    </div>
   )
 }
 
